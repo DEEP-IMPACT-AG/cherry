4ef2b59f0a0c070f6572fcc8c13c208614b8bf65
Luan Gjokaj <luan@riangle.com>
2019-01-19 17:01:06 +0100

    New design

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,14 +2,34 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import styles from './Nav.css';
 import logo from '../../assets/images/logo.svg';
+import NavItem from './NavItem';
 
 function Nav() {
 	return (
-		<div className={styles.navigation}>
+		<header className={styles.navigation}>
 			<Link to="/" className={styles.logo}>
 				<img src={logo} alt="Logo" />
+				<h1>Cherry - Design System</h1>
+				<span>Design System</span>
 			</Link>
-		</div>
+			<ul className={styles.list}>
+				<NavItem title="Overview">
+					<ul className={styles.sub}>
+						<li><a href="#">Introduction</a></li>
+						<li><a href="#">Code Guidelines</a></li>
+					</ul>
+				</NavItem>
+				<li>
+					<a href="#">Design</a>
+				</li>
+				<li>
+					<a href="#">CSS</a>
+				</li>
+				<li>
+					<a href="#">Components</a>
+				</li>
+			</ul>
+		</header>
 	);
 }
 
