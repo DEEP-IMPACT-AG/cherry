b1c4950e290df84ff215dc3f27c17c94e69d8fd1
Luan Gjokaj <luan@riangle.com>
2019-01-19 23:41:46 +0100

    Add icons

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,6 +4,9 @@ import Content from '../../Components/Content';
 import Wrapper from '../../Components/Wrapper';
 import styles from './Home.css';
 const data = require('./data.md');
+import Sketch from './Logos/Sketch.svg';
+import PostCSS from './Logos/PostCSS.svg';
+import ReactLogo from './Logos/React.svg';
 
 function Home({ match }) {
 	return (
@@ -12,6 +15,17 @@ function Home({ match }) {
 			<Content>
 				<Wrapper>
 					<div className={styles.content} dangerouslySetInnerHTML={{ __html: data.__content }} />
+					<ul className={styles.technologies}>
+						<li>
+							<img src={Sketch} alt="Sketch"/>
+						</li>
+						<li>
+							<img src={PostCSS} alt="PostCSS"/>
+						</li>
+						<li>
+							<img src={ReactLogo} alt="React"/>
+						</li>
+					</ul>
 				</Wrapper>
 			</Content>
 		</div>
