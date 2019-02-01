50cbb0884ce9c38ad60bd8bd88887b87e3b4461c
Luan Gjokaj <luan@riangle.com>
2019-02-01 16:28:14 +0100

    Text component: return correct elements (small or blockquote)

--- src/Components/Layout/Text/Text.js
+++ src/Components/Layout/Text/Text.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import styles from './Text.css';
 
-function Text({ children, className, textAlign, eyebrow, subtitle, small }) {
-	return (
-		<div
-			className={classNames(
-				{
-					[styles.eyebrow]: eyebrow,
-					[styles.subtitle]: subtitle,
-					[styles.textLeft]: textAlign === 'left',
-					[styles.textCenter]: textAlign === 'center',
-					[styles.textRight]: textAlign === 'right',
-					[styles.small]: small,
-				},
-				styles.text,
-			)}
-		>
-			{children}
-		</div>
-	);
+class Text extends Component {
+	render() {
+		const { children, className, textAlign, eyebrow, subtitle, small, blockquote } = this.props;
+		const ownClassName = classNames(
+			{
+				[styles.eyebrow]: eyebrow,
+				[styles.subtitle]: subtitle,
+				[styles.textLeft]: textAlign === 'left',
+				[styles.textCenter]: textAlign === 'center',
+				[styles.textRight]: textAlign === 'right',
+				[styles.small]: small,
+				[styles.blockquote]: blockquote,
+			},
+			styles.text,
+		);
+
+		if (small) {
+			return <small className={ownClassName}>{children}</small>;
+		}
+
+		if (blockquote) {
+			return <blockquote className={ownClassName}>{children}</blockquote>;
+		}
+
+		return <div className={ownClassName}>{children}</div>;
+	}
 }
 
 export default Text;
