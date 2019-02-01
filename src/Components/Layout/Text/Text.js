aa1a1ce318b5438e42bf3510e28c2edfa04c2c0a
Luan Gjokaj <luan@riangle.com>
2019-02-01 15:39:04 +0100

    Added textAlign property for the Text component

--- src/Components/Layout/Text/Text.js
+++ src/Components/Layout/Text/Text.js
@@ -2,12 +2,15 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './Text.css';
 
-function Text({ children, className, eyebrow, subtitle }) {
+function Text({ children, className, textAlign, eyebrow, subtitle }) {
 	return (
 		<div
 			className={classNames(
 				{
 					[styles.eyebrow]: eyebrow,
+					[styles.textLeft]: textAlign === 'left',
+					[styles.textCenter]: textAlign === 'center',
+					[styles.textRight]: textAlign === 'right',
 				},
 				styles.text,
 			)}
