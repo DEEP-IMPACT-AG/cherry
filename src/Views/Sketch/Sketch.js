916e52a2c82d4b0531b92aff81e1b656845c1f35
Luan Gjokaj <luan@riangle.com>
2019-05-17 13:49:20 +0200

    📦 NEW: Typography preview

--- src/Views/Sketch/Sketch.js
+++ src/Views/Sketch/Sketch.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Head from '../../Components/Head';
-import { ContentPusher, Container, Readable, Button } from '../../Components/Layout';
+import { ContentPusher, Container, Readable, Button, Row, Col } from '../../Components/Layout';
 import { Sketch as SketchIcon } from '../../assets/svg';
 import styles from './Sketch.css';
 const data = require('./data-sketch.md');
@@ -33,6 +33,24 @@ class Sketch extends Component {
 								className={styles.content}
 								dangerouslySetInnerHTML={{ __html: data.__content }}
 							/>
+							<Row className={styles.images}>
+								<Col xs={4}>
+									<a href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1558092359/cherry/typography/1-typography-primary.jpg" target="_blank" title="Preview">
+										<img src="https://res.cloudinary.com/deep-impact-ag/image/upload/v1558092359/cherry/typography/1-typography-primary.jpg" alt="Cherry Primary Typography"/>
+									</a>
+								</Col>
+								<Col xs={4}>
+									<a href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1558092359/cherry/typography/2-typography-secondary.jpg" target="_blank" title="Preview">
+										<img src="https://res.cloudinary.com/deep-impact-ag/image/upload/v1558092359/cherry/typography/2-typography-secondary.jpg" alt="Cherry Secondary Typography"/>
+									</a>
+								</Col>
+								<Col xs={4}>
+									<a href="https://res.cloudinary.com/deep-impact-ag/image/upload/v1558092359/cherry/typography/3-typography-tertiary.jpg" target="_blank" title="Preview">
+										<img src="https://res.cloudinary.com/deep-impact-ag/image/upload/v1558092359/cherry/typography/3-typography-tertiary.jpg" alt="Cherry Tertiary Typography"/>
+									</a>
+								</Col>
+							</Row>
+							
 							<Button
 								href="https://drive.google.com/file/d/1p2CrJEANwMy2VeoexkH51cun847shgij/view?usp=sharing"
 								target="_blank"
