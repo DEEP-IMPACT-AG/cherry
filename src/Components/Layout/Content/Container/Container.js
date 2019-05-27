1437cdd9c95094d538a96002acac52dc5821b214
Luan Gjokaj <luan@riangle.com>
2019-05-27 22:50:48 +0200

    📦 NEW: Add Grid components to lib

--- src/Components/Layout/Content/Container/Container.js
+++ src/Components/Layout/Content/Container/Container.js
@@ -2,20 +2,23 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './Container.css';
 
-function Container({ className, children, fluid = false  }) {
-	return (
-		<div
-			className={classNames(
-				styles.container,
-				{
-					[styles.containerFluid]: fluid,
-				},
-				className
-			)}
-		>
-			{children}
-		</div>
-	);
+export class Container extends React.Component {
+	render () {
+		const { className, children, fluid = false } = this.props;
+		return (
+			<div
+				className={classNames(
+					styles.container,
+					{
+						[styles.containerFluid]: fluid,
+					},
+					className
+				)}
+			>
+				{children}
+			</div>
+		);
+	}
 }
 
 export default Container;
