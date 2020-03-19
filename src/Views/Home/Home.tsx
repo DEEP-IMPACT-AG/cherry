a364ff02c561c326231bac9acd6fa0fd1e60b7b4
Luan <luan@riangle.com>
2020-03-19 23:48:03 +0100

    🐛 FIX: Revert

--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Head from "../../Components/Head";
-import { ContentPusher, Container, Readable, Row, Col } from "../../Components/Layout";
+import { ContentPusher, Container, Readable } from "../../Components/Layout";
 const styles = require("./Home.css");
 const data = require("./data-home.md");
 
@@ -12,37 +12,12 @@ class Home extends Component {
 				<ContentPusher>
 					<Container>
 						<Readable noOrderedLists>
-							<div className={styles.limit}>
-								<h1>Introduction</h1>
-								<h4>
-									Cherry is a live inventory of definitions
-									for the UI components, design patterns,
-									naming conventions, brand assets and code
-									guidelines.
-								</h4>
-								<p>
-									Cherry is technology agnostic – meaning that
-									it can be applied independently of the
-									technology stack. It defines the underlying
-									design philosophy of your project and serves
-									as the bridge between design and code. It is
-									bundled with all the right tools you need to
-									get started as fast as possible.
-								</p>
-							</div>
-							<Row className={styles.row}>
-								<Col xs={12} lg={8} className={styles.col}>
-									<Row className={styles.row}>
-										<Col xs={12} lg={6} className={styles.col}>
-											Hello
-										</Col>
-										<Col xs={12} lg={6} className={styles.col}>
-											Hello2
-										</Col>
-									</Row>
-								</Col>
-								<Col xs={12} lg={4} className={styles.col}>2</Col>
-							</Row>
+							<div
+								className={styles.content}
+								dangerouslySetInnerHTML={{
+									__html: data.__content,
+								}}
+							/>
 						</Readable>
 					</Container>
 				</ContentPusher>
