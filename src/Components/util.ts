641ee18d950a5652e2d9c7029c0507f1dce5e19c
Luan <luan@riangle.com>
2020-05-05 14:45:39 +0200

    👌 IMPROVE: Transition timing

--- src/Components/util.ts
+++ src/Components/util.ts
@@ -2,7 +2,7 @@ export const clickEvent = (event: any, to: any, push: any) => {
 	const contentPusher: any = document.getElementById("content-pusher");
 	contentPusher.classList.remove("animateIn");
 	contentPusher.classList.add("animateOut");
-	setTimeout(() => contentPusher.classList.add("animateIn"), 500);
+	setTimeout(() => contentPusher.classList.add("animateIn"), 300);
 	event.preventDefault();
-	setTimeout(() => push(to), 500);
+	setTimeout(() => push(to), 300);
 };
